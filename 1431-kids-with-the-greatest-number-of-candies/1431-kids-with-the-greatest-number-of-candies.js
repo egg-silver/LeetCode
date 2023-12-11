ace0f5a43849a79e49fa5e024e855c673d5b0c49
Jieun Lee <133847649+egg-silver@users.noreply.github.com>
2023-12-12 02:04:49 +0900

    Update 1431-kids-with-the-greatest-number-of-candies.js

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.js
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.js
@@ -1,3 +1,12 @@
+var kidsWithCandies = function (candies, extraCandies) {
+  let max = Math.max(...candies);
+  let arr = candies.map((a) => a + extraCandies);
+  return arr.map((a) => {
+    return a < max ? false : true;
+  });
+};
+
+//더 효율적인 방법
 var kidsWithCandies = function(candies, extraCandies) {
   let max = Math.max(...candies);
   return candies.map(a => a + extraCandies >= max);
